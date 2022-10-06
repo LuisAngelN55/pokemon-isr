d8ec2deef99e9ffb5235f27966d1364080fa62a5
LuisMoov <lang@zuntrix.com>
2022-10-06 10:14:14 -0500

    40. NextUI - React UI Library

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@nextui-org/react'
+import type { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Button color="gradient">
+        Hola a todos
+      </Button>
+    </>
+  )
+}
+
+export default HomePage
