e4388de65e4a71eacb02e2d8a7ff810a028ebc9a
Luis Angel <lang@zuntrix.com>
2022-10-09 17:34:52 -0500

    Fin secciòn 4: Pokemon static

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from '../../components/layouts';
+
+const FavirtesPage = () => {
+  return (
+    <MainLayout title='Pokèmons - Favoritos'>
+        <h1>Favoritos</h1>
+    </MainLayout>
+  )
+}
+
+export default FavirtesPage;
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,22 +1,87 @@
 import { MainLayout } from "../../components/layouts"
 import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { Pokemon } from "../../interfaces";
+import { pokeApi } from "../../api";
+import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
 
 interface Props {
-  // pokemon: any;
-  id: string;
-  name: string;
+  pokemon: Pokemon
 }
 
 
-const PokemonPage: NextPage<Props> = ({ id, name }) => {
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
-  const router = useRouter();
-  console.log(router.query);
+  //   SABER RUTA ACTUAL (URL)
+  // const router = useRouter();
+  // console.log(router.query);
+
 
   return (
     <MainLayout title="Algún Pokémon">
-      <h1>{ id } - { name }</h1>
+      <Grid.Container css={{ marginTop: '5px'}} gap={ 2 } >
+        <Grid xs={ 12 } sm={ 4 } >
+          <Card isHoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image
+                src={ pokemon.sprites.other?.dream_world.front_default || '/no-image.png' }
+                alt={ pokemon.name }
+                width="100%"
+                height={ 200 }
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+
+        <Grid xs={ 12 } sm={ 8 }>
+          <Card>
+            <Card.Header css={{ display: 'flex', justifyContent:'space-between'}}>
+              <Text h1 transform="capitalize">{ pokemon.name }</Text>
+              
+              <Button
+                color="gradient"
+                ghost
+              >
+                Guardar en favoritos
+              </Button>
+            </Card.Header>
+
+            <Card.Body>
+              <Text size={ 30 }>Sprites:</Text>
+              <Container direction="row" display="flex" gap={0}>
+                <Image
+                  src={ pokemon.sprites.front_default }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height= { 100 }
+                />
+                
+                <Image
+                  src={ pokemon.sprites.back_default }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height= { 100 }
+                />
+                <Image
+                  src={ pokemon.sprites.front_shiny }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height= { 100 }
+                />
+                <Image
+                  src={ pokemon.sprites.back_shiny }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height= { 100 }
+                />
+
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+
+      </Grid.Container>
+      
     </MainLayout> 
   )
 };
@@ -26,27 +91,26 @@ const PokemonPage: NextPage<Props> = ({ id, name }) => {
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
+  const pokemon151 = [...Array(151)].map( ( value, index ) => `${ index + 1 }`);
+
   return {
-    paths: [
-      {
-        params: {
-          id: '1'
-        }
-      }
-    ],
+    paths: pokemon151.map( id => ({
+      params: { id }
+    })),
     fallback: false
   }
 }
 
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { id } = ctx.params as { id: string };
   
-  // const { data }= await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+  const { data }= await pokeApi.get<Pokemon>(`/pokemon/${ id }`);
 
   return {
     props: {
-      id: 1,
-      name: 'Bulbasaur'
+      pokemon: data
     }
   }
 }
